e9975a6519de30897e4ff7c705046482992a4337
Michael Kage <michaelkage9@gmail.com>
2026-08-24 17:39:15 +0100

    Wire student portal page to ViewModel

--- BombiHighSchoolWindowsApp/BombiHighSchool.App/Views/StudentPortalPage.xaml.cs
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/Views/StudentPortalPage.xaml.cs
@@ -1,14 +1,19 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using BombiHighSchool.App.ViewModels;
 
 namespace BombiHighSchool.App.Views;
 
 public sealed partial class StudentPortalPage : Page
 {
+    public StudentPortalViewModel ViewModel { get; } = new();
+
     public StudentPortalPage()
     {
         InitializeComponent();
     }
+
+    public async Task LoadStudentAsync(string studentId) => await ViewModel.LoadAsync(studentId);
 
     private void SignOut_Click(object sender, RoutedEventArgs e)
         => Frame?.Navigate(typeof(LoginPage));
