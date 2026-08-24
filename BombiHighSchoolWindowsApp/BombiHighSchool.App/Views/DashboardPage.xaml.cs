72ab3be7d6cec27c298b86df8254c504fa523257
Michael Kage <michaelkage9@gmail.com>
2026-08-24 17:23:41 +0100

    Add dashboard page code-behind

---
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/Views/DashboardPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace BombiHighSchool.App.Views;
+
+public sealed partial class DashboardPage : Page
+{
+    public DashboardPage()
+    {
+        InitializeComponent();
+    }
+}
