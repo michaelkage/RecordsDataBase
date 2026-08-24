7457fd1ff77a9859025e8c4f52ca52b86f1a636b
Michael Kage <michaelkage9@gmail.com>
2026-08-24 17:27:44 +0100

    Wire native Subjects Scores and Rankings navigation

--- BombiHighSchoolWindowsApp/BombiHighSchool.App/MainPage.xaml.cs
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/MainPage.xaml.cs
@@ -6,47 +6,23 @@ namespace BombiHighSchool.App;
 
 public sealed partial class MainPage : Page
 {
-    public MainPage()
+    public MainPage() { InitializeComponent(); Loaded += MainPage_Loaded; }
+    private void MainPage_Loaded(object sender, RoutedEventArgs e) { AppNavigation.SelectedItem = AppNavigation.MenuItems[0]; ContentFrame.Navigate(typeof(DashboardPage)); }
+    private void Navigation_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
     {
-        InitializeComponent();
-        Loaded += MainPage_Loaded;
+        if (args.SelectedItem is NavigationViewItem item && item.Tag is string section) NavigateToSection(section);
+        else if (args.SelectedItemContainer is NavigationViewItem container && container.Tag is string footerSection) NavigateToSection(footerSection);
     }
-
-    private void MainPage_Loaded(object sender, RoutedEventArgs e)
-    {
-        AppNavigation.SelectedItem = AppNavigation.MenuItems[0];
-        ContentFrame.Navigate(typeof(DashboardPage));
-    }
-
-    private void Navigation_SelectionChanged(
-        NavigationView sender,
-        NavigationViewSelectionChangedEventArgs args)
-    {
-        if (args.SelectedItem is NavigationViewItem item && item.Tag is string section)
-        {
-            NavigateToSection(section);
-            return;
-        }
-
-        if (args.SelectedItemContainer is NavigationViewItem container && container.Tag is string footerSection)
-        {
-            NavigateToSection(footerSection);
-        }
-    }
-
     private void NavigateToSection(string section)
     {
         switch (section)
         {
-            case "Dashboard":
-                ContentFrame.Navigate(typeof(DashboardPage));
-                break;
-            case "Students":
-                ContentFrame.Navigate(typeof(StudentsPage));
-                break;
-            default:
-                ContentFrame.Navigate(typeof(DashboardPage));
-                break;
+            case "Dashboard": ContentFrame.Navigate(typeof(DashboardPage)); break;
+            case "Students": ContentFrame.Navigate(typeof(StudentsPage)); break;
+            case "Subjects": ContentFrame.Navigate(typeof(SubjectsPage)); break;
+            case "Scores": ContentFrame.Navigate(typeof(ScoresPage)); break;
+            case "Rankings": ContentFrame.Navigate(typeof(RankingsPage)); break;
+            default: ContentFrame.Navigate(typeof(DashboardPage)); break;
         }
     }
 }
