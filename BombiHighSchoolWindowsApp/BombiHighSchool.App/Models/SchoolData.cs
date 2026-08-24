2c92d4b19a8d2213836485e365c32097e1b5dd87
Michael Kage <michaelkage9@gmail.com>
2026-08-24 17:39:39 +0100

    Store explicit subject enrollments locally

--- BombiHighSchoolWindowsApp/BombiHighSchool.App/Models/SchoolData.cs
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/Models/SchoolData.cs
@@ -5,6 +5,7 @@ public class SchoolData
     public List<Student> Students { get; set; } = [];
     public List<StudentDetails> StudentDetails { get; set; } = [];
     public List<Subject> Subjects { get; set; } = [];
+    public List<SubjectEnrollment> Enrollments { get; set; } = [];
     public List<Score> Scores { get; set; } = [];
     public List<UserAccount> Users { get; set; } = [];
 }
