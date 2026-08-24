0fef90b49044f84f68ceaaee356399e10c6cdaf1
Michael Kage <michaelkage9@gmail.com>
2026-08-24 17:52:55 +0100

    Expose native main window for Windows printing and clear sessions on close

--- BombiHighSchoolWindowsApp/BombiHighSchool.App/MainWindow.xaml.cs
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/MainWindow.xaml.cs
@@ -5,9 +5,13 @@ namespace BombiHighSchool.App;
 
 public sealed partial class MainWindow : Window
 {
+    public static MainWindow? Current { get; private set; }
+
     public MainWindow()
     {
+        Current = this;
         InitializeComponent();
         RootFrame.Navigate(typeof(LoginPage));
+        Closed += (_, _) => Services.SessionService.Clear();
     }
 }
