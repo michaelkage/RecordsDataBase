ece0e57f5920c57304109c7427844dc318959370
Michael Kage <michaelkage9@gmail.com>
2026-08-24 17:29:07 +0100

    Add local authentication account model

---
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/Models/UserAccount.cs
@@ -0,0 +1,11 @@
+namespace BombiHighSchool.App.Models;
+
+public class UserAccount
+{
+    public string Username { get; set; } = "";
+    public string Role { get; set; } = "";
+    public string StudentId { get; set; } = "";
+    public string PasswordHash { get; set; } = "";
+    public string PasswordSalt { get; set; } = "";
+    public bool MustChangePassword { get; set; }
+}
