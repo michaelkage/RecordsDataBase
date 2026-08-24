7fd6466190f5359327c29077670d898127f17b1a
Michael Kage <michaelkage9@gmail.com>
2026-08-24 17:25:32 +0100

    Expand local school database model

--- BombiHighSchoolWindowsApp/BombiHighSchool.App/Models/SchoolData.cs
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/Models/SchoolData.cs
@@ -3,4 +3,6 @@ namespace BombiHighSchool.App.Models;
 public class SchoolData
 {
     public List<Student> Students { get; set; } = [];
+    public List<Subject> Subjects { get; set; } = [];
+    public List<Score> Scores { get; set; } = [];
 }
