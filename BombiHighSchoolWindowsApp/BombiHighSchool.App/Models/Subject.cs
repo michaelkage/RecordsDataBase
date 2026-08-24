38af7e2f6631f0059925d16df362ffd3124c90c1
Michael Kage <michaelkage9@gmail.com>
2026-08-24 17:25:23 +0100

    Add local Subject model

---
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/Models/Subject.cs
@@ -0,0 +1,10 @@
+namespace BombiHighSchool.App.Models;
+
+public class Subject
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public string Code { get; set; } = "";
+    public string Department { get; set; } = "";
+    public bool IsCompulsory { get; set; }
+}
