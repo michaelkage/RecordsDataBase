a4b1778cd036cd1ee44ab7e5efefd5d59566ab3a
Michael Kage <michaelkage9@gmail.com>
2026-08-24 17:25:47 +0100

    Add richer local student details model

---
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/Models/StudentDetails.cs
@@ -0,0 +1,15 @@
+namespace BombiHighSchool.App.Models;
+
+public class StudentDetails
+{
+    public string StudentId { get; set; } = "";
+    public string Arm { get; set; } = "";
+    public string Department { get; set; } = "";
+    public string DateOfBirth { get; set; } = "";
+    public string AdmissionNumber { get; set; } = "";
+    public string ParentName { get; set; } = "";
+    public string ParentPhone { get; set; } = "";
+    public string Address { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Status { get; set; } = "Active";
+}
