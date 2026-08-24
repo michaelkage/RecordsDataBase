2f83a022b4979abae81c824f378d26f80ea0e17e
Michael Kage <michaelkage9@gmail.com>
2026-08-24 17:25:52 +0100

    Add richer student details to local database

--- BombiHighSchoolWindowsApp/BombiHighSchool.App/Models/SchoolData.cs
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/Models/SchoolData.cs
@@ -3,6 +3,7 @@ namespace BombiHighSchool.App.Models;
 public class SchoolData
 {
     public List<Student> Students { get; set; } = [];
+    public List<StudentDetails> StudentDetails { get; set; } = [];
     public List<Subject> Subjects { get; set; } = [];
     public List<Score> Scores { get; set; } = [];
 }
