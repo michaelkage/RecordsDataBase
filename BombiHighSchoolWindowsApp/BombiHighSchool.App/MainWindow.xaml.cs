64e21b00511a99a9626b08402903548438acb8ef
Michael Kage <michaelkage9@gmail.com>
2026-08-24 17:30:28 +0100

    Start app at separate portal login selection

--- BombiHighSchoolWindowsApp/BombiHighSchool.App/MainWindow.xaml.cs
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml;
+using BombiHighSchool.App.Views;
 
 namespace BombiHighSchool.App;
 
@@ -7,5 +8,6 @@ public sealed partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        RootFrame.Navigate(typeof(LoginPage));
     }
 }
