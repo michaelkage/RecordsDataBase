34bfa0456fd283270ed8264edf28f018af528516
Michael Kage <michaelkage9@gmail.com>
2026-08-25 20:08:29 +0100

    Add class arm and term ranking filters

--- BombiHighSchoolWindowsApp/BombiHighSchool.App/ViewModels/RankingsViewModel.cs
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/ViewModels/RankingsViewModel.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using BombiHighSchool.App.Models;
 using BombiHighSchool.App.Services;
 
 namespace BombiHighSchool.App.ViewModels;
@@ -8,9 +9,28 @@ namespace BombiHighSchool.App.ViewModels;
 public partial class RankingsViewModel : ObservableObject
 {
     private readonly RankingService _service = new();
+    private readonly LocalDataService _dataService = new();
     [ObservableProperty] private ObservableCollection<StudentRanking> rankings = [];
     [ObservableProperty] private string classFilter = "All classes";
+    [ObservableProperty] private string armFilter = "All arms";
+    [ObservableProperty] private string session = "2026/2027";
+    [ObservableProperty] private string term = "First Term";
     public string[] Classes { get; } = ["All classes", "JSS1", "JSS2", "JSS3", "SS1", "SS2", "SS3"];
-    [RelayCommand] public async Task LoadAsync() { var filter = ClassFilter == "All classes" ? null : ClassFilter; Rankings = new(await _service.GetStudentRankingsAsync(filter)); }
+    public string[] Arms { get; } = ["All arms", .. SchoolRules.Arms];
+    public string[] Terms => SchoolRules.Terms;
+
+    [RelayCommand]
+    public async Task LoadAsync()
+    {
+        var data = await _dataService.LoadAsync();
+        Session = data.CurrentAcademicPeriod.Session;
+        Term = data.CurrentAcademicPeriod.Term;
+        var classFilter = ClassFilter == "All classes" ? null : ClassFilter;
+        var armFilter = ArmFilter == "All arms" ? null : ArmFilter;
+        Rankings = new(await _service.GetStudentRankingsAsync(classFilter, armFilter, Session, Term));
+    }
+
     partial void OnClassFilterChanged(string value) => _ = LoadAsync();
+    partial void OnArmFilterChanged(string value) => _ = LoadAsync();
+    partial void OnTermChanged(string value) => _ = LoadAsync();
 }
