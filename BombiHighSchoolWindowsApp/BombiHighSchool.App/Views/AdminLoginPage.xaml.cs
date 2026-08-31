89231cc86a3b24c89a4ed718b541ca6f725f41ed
Michael Kage <michaelkage9@gmail.com>
2026-08-31 11:38:57 +0100

    Wire admin login page to application AuthenticationService

--- BombiHighSchoolWindowsApp/BombiHighSchool.App/Views/AdminLoginPage.xaml.cs
+++ BombiHighSchoolWindowsApp/BombiHighSchool.App/Views/AdminLoginPage.xaml.cs
@@ -1,17 +1,20 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using BombiHighSchool.App.Services;
 using BombiHighSchool.App.ViewModels;
 
 namespace BombiHighSchool.App.Views;
 
 public sealed partial class AdminLoginPage : Page
 {
-    public AdminLoginViewModel ViewModel { get; } = new();
+    public AdminLoginViewModel ViewModel { get; }
 
     public AdminLoginPage()
     {
         InitializeComponent();
+        ViewModel = new AdminLoginViewModel(((App)Application.Current).Services.GetRequiredService<AuthenticationService>());
         ViewModel.LoginSucceeded += LoginSucceeded;
+        DataContext = ViewModel;
     }
 
     private void Password_Changed(object sender, RoutedEventArgs e)
